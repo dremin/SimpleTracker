104ab4cff41149fa17c069e09acb36cc1f583c2a
Sam Johnson <sam@sams-macbook-air.local>
2018-12-22 21:13:23 +0000

    Initial Commit

---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Sam Johnson on 12/22/18.
+//  Copyright © 2018 Sam Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
